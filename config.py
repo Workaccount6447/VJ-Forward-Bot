e8adbc7b68fcb3ecda738b8a7e0f68b65d2833ba
Workaccount6447 <sndj86779@gmail.com>
2025-12-12 08:57:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "2d14a3ad7a0ea5ec9167fb1623f56fa2")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "RoyalityBots") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://dealsduniyalimited_db_user:RwEd6eiGaWdeRVzx@cluster0.pg8j47e.mongodb.net/?appName=Cluste")
+    DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "RoyalityBots")
     BOT_OWNER = int(environ.get("BOT_OWNER", "8420494874"))
 
@@ -27,4 +27,5 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
